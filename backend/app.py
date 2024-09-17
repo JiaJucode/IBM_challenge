d09e7fdcabd6c97e5557f256741d5fc11fb35d1e
JiaJu <jiaju225020@gmail.com>
2024-09-17 02:59:34 +0100

    add backend chat history log

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,17 @@
 ## Flask APIs. Return JSON data
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import sqlite3
+from session import ChatHistoryDatabase
+import json
+
 app = Flask(__name__)
+
+CORS(app)
+
+def aiResponse(message):
+    return "I am a bot"
 
 # Enable CORS for requests only to /api/* from frontend server
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
@@ -11,5 +20,41 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 def hello():
     return jsonify({'message': 'Hello World!'})
 
+def get_chat_history(chat_ID):
+    print(chat_ID)
+    db = ChatHistoryDatabase()
+    chat_history = db.get_chat_history(chat_ID)[0]
+    print(json.loads(chat_history))
+    return jsonify({'chat_history': chat_history})
+
+def get_user_chats(user_ID):
+    chat = ChatHistoryDatabase().get_user_chats(user_ID)
+    chat_IDs, chat_names = zip(*chat) if chat else ([], [])
+    return jsonify([{'chat_ID': chat_ID, 'chat_name': chat_name} 
+                    for chat_ID, chat_name in zip(chat_IDs, chat_names)])
+
+@app.route('/api/ChatHistory', methods=['GET'])
+def handle_get_chat_history():
+    chat_ID = request.args.get('chat_ID')
+    user_ID = request.args.get('user_ID')
+    if chat_ID:
+        return get_chat_history(chat_ID)
+    elif user_ID:
+        return get_user_chats(user_ID)
+    else:
+        return jsonify({'error': 'chat_ID or user_ID parameter is required'})
+
+@app.route('/api/AIResponse/<int:user_ID>/<int:chat_ID>', methods=['POST'])
+def add_chat_message(chat_ID):
+    message = request.json['message']
+    chat_history = ChatHistoryDatabase()
+    chat_history.add_message(chat_ID, message)
+    response = aiResponse(message)
+    chat_history.add_message(chat_ID, response)
+    # not closing the database connection to reduce overhead
+    return jsonify({'response': response})
+
+
 if __name__ == '__main__':
+    db = ChatHistoryDatabase()
     app.run(host='0.0.0.0', port=5000, debug=True)
--- backend/session.py
+++ backend/session.py
@@ -1,4 +1,50 @@
 import sqlite3
+import json
+
+example_chat = [
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+            "questions",
+            '''blablbalsdfl absldfblab sl fbalsfbflabs ldffblabs dflbas fhjkasdf kjj askdf hjk sahkjf gasdgf
+            blablbals dflabsl dfblabslfbal sfbfl absldf fblabsd flbasfh jkasdf kjjas dfhjksah kjfgasdgf
+            blablbals dflabsldf blab slfbalsf bflabsldffblab sdflb asf hjka sdfkjjaskdf hjksa hkjfgasdgf
+            blablbalsdf labsldf blabslfbalsf bflabsldffblabsdflbasfhjkasdfkjjas kdfhj ksahkjfgasdgf
+            blablbalsdflabsldfbl absl fbalsfbflabsldff blabsdflbasfhj kas dfkjjas kdfhjksah kjfgasdgf
+            blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
+    ]
 
 class Database:
     def __init__(self, db_path:str) -> None:
@@ -32,6 +78,72 @@ class Session:
     def __init__(self) -> None:
         pass
 
+# feel free to restructure the code
+# TODO: user authentication
+# TODO: redesign for fast adding and lazy loading
+class ChatHistoryDatabase:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect("backend/chat_history.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            '''create table if not exists UserChats (
+                user_ID INTEGER,
+                chat_ID INTEGER,
+                chat_name TEXT,
+                PRIMARY KEY (user_ID, chat_ID)
+                )'''
+        )
+        # messages are stored as json strings
+        self.cursor.execute(
+            '''create table if not exists ChatHistory (
+                chat_ID INTEGER PRIMARY KEY,
+                messages TEXT
+                )'''
+        )
+        self.connection.commit()
+        self.next_chat_ID = 0
+        self.next_user_ID = 0
+
+    def get_next_chat_ID(self):
+        self.next_chat_ID += 1
+        return self.next_chat_ID
+    
+    def get_next_user_ID(self):
+        self.next_user_ID += 1
+        return self.next_user_ID
+
+    def add_message(self, chat_ID, new_message):
+        # get the chat history
+        chat_history = self.cursor.execute(f"select messages from ChatHistory where chat_ID={chat_ID}")\
+                            .fetchone()
+        if not chat_history:
+            raise ValueError("Chat ID does not exist")
+        try:
+            json_chat_history = json.loads(chat_history[0]) if len(chat_history[0]) > 0 else []
+        except json.JSONDecodeError:
+            print("history is corrupted")
+            json_chat_history = []
+        json_chat_history.append(new_message)
+        chat_history_str = json.dumps(json_chat_history)
+        self.cursor.execute(f"UPDATE ChatHistory SET messages='{chat_history_str}' where chat_ID={chat_ID}")
+        self.connection.commit()
+
+    def add_chat(self, user_ID, chat_ID, chat_name=""):
+        self.cursor.execute('''INSERT OR IGNORE INTO UserChats (user_ID, chat_ID, chat_name) 
+                                VALUES (?, ?, ?)''', (user_ID, chat_ID, chat_name))
+        self.cursor.execute(f"INSERT OR IGNORE INTO ChatHistory (chat_ID, messages) VALUES ({chat_ID}, '')")
+        self.connection.commit()
+
+    def get_chat_history(self, chat_ID):
+        return self.cursor.execute(f"select messages from ChatHistory where chat_ID={chat_ID}").fetchone()
+
+    def get_user_chats(self, user_ID):
+        return self.cursor.execute(f"select chat_ID, chat_name from UserChats where user_ID={user_ID}").fetchall()
+    
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+
 # try out database
 if __name__ == "__main__":
     db = Database("backend/data.db")
@@ -44,4 +156,14 @@ if __name__ == "__main__":
         db.add_history(term, response)
     print(2, db("select * from History").fetchall())
     db.connection.close()
+
+
+    db = ChatHistoryDatabase()
+    db.add_chat(1, 1, "test chat")
+    for message in example_chat:
+        db.add_message(1, message)
+    print(json.loads(db.get_chat_history(1)[0]))
+    db.close()
+
+
     
