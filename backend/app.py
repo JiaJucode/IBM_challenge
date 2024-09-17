ccd4d9f944500ae7ebb9463ded274031e723a34d
Xinyang Chen <xinyangchen4@gmail.com>
2024-09-17 15:37:34 +0100

    Use inheritance base class for databases

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 ## Flask APIs. Return JSON data
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
-import sqlite3
-from session import ChatHistoryDatabase
+from session import ChatHistoryDatabase, SearchResponseDatabase
 import json
 
 app = Flask(__name__)
+chat_db = ChatHistoryDatabase()
+search_db = SearchResponseDatabase()
 
 def aiResponse(message):
     return "I am a bot"
@@ -19,18 +19,18 @@ def hello():
     return jsonify({'message': 'Hello World!'})
 
 def get_chat_history(chat_ID):
-    db = ChatHistoryDatabase()
-    chat_history = db.get_chat_history(chat_ID)[0]
+    chat_history = chat_db.get_chat_history(chat_ID)[0]
     return jsonify({'chat_history': chat_history})
 
 def get_user_chats(user_ID):
-    chat = ChatHistoryDatabase().get_user_chats(user_ID)
+    chat = chat_db.get_user_chats(user_ID)
     chat_IDs, chat_names = zip(*chat) if chat else ([], [])
     return jsonify([{'chat_ID': chat_ID, 'chat_name': chat_name} 
                     for chat_ID, chat_name in zip(chat_IDs, chat_names)])
 
 @app.route('/api/ChatHistory', methods=['GET'])
 def handle_get_chat_history():
+    chat_db.start()
     chat_ID = request.args.get('chat_ID')
     user_ID = request.args.get('user_ID')
     if chat_ID:
@@ -44,14 +44,13 @@ def handle_get_chat_history():
 def add_chat_message():
     chat_ID = request.json['chat_ID']
     message = request.json['message']
-    chat_history = ChatHistoryDatabase()
-    chat_history.add_message(chat_ID, message)
+    chat_db.start()
+    chat_db.add_message(chat_ID, message)
     response = aiResponse(message)
-    chat_history.add_message(chat_ID, response)
+    chat_db.add_message(chat_ID, response)
     # not closing the database connection to reduce overhead
+    chat_db.save_exit()
     return jsonify({'response': response})
 
-
 if __name__ == '__main__':
-    db = ChatHistoryDatabase()
     app.run(host='0.0.0.0', port=5000, debug=True)
--- backend/session.py
+++ backend/session.py
@@ -46,8 +46,13 @@ example_chat = [
             blablbal sdfl absldfblab slfbalsfbflabsl ff blabsdflb asfh jkasdfkjjaskdfhjk sahkjfgasdgf''',
     ]
 
+class Session:
+    def __init__(self) -> None:
+        pass
+
 class Database:
-    def __init__(self, db_path:str) -> None:
+    # Base class for all databases
+    def __init__(self, db_path="backend/data.db") -> None:
         self.name = db_path
 
     def start(self):
@@ -56,36 +61,38 @@ class Database:
     def __call__(self, query):
         return self.connection.execute(query)
 
-    def create_history_table(self):
-        self("drop table if exists History")
-        self("create table History (search_term TEXT, frequency INTEGER, response TEXT)")
-
-    def add_history(self, term, response=""):
-        entry = self(f"select frequency, response from History where search_term='{term}'").fetchone()
-        if entry and entry[0]:
-            # cache hit, increment frequency and return cached response
-            self(f"update History set frequency={int(entry[0])+1} where search_term='{term}'")
-            return entry[1]
-        else:
-            # no matching history, create new one
-            self(f"insert into History (search_term, frequency, response) values ('{term}', 1, '{response}')")
+    def create_table(self):
+        pass
 
     def save_exit(self):
         self.connection.commit()
         self.connection.close()
 
-class Session:
-    def __init__(self) -> None:
-        pass
+class SearchResponseDatabase(Database):
+    def create_table(self):
+        self("create table if not exists Search (search_term TEXT, frequency INTEGER, response TEXT)")
+
+    def add_search(self, term, response=""):
+        entry = self(f"select frequency, response from Search where search_term='{term}'").fetchone()
+        if entry and entry[0]:
+            # cache hit, increment frequency and return cached response
+            self(f"update Search set frequency={int(entry[0])+1} where search_term='{term}'")
+            return entry[1]
+        else:
+            # no matching history, create new one
+            self(f"insert into Search (search_term, frequency, response) values ('{term}', 1, '{response}')")
 
 # feel free to restructure the code
 # TODO: user authentication
 # TODO: redesign for fast adding and lazy loading
-class ChatHistoryDatabase:
-    def __init__(self) -> None:
-        self.connection = sqlite3.connect("backend/chat_history.db")
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(
+class ChatHistoryDatabase(Database):
+    def start(self) -> None:
+        self.next_chat_ID = 0
+        self.next_user_ID = 0
+        super().start()
+
+    def create_table(self):
+        self(
             '''create table if not exists UserChats (
                 user_ID INTEGER,
                 chat_ID INTEGER,
@@ -94,15 +101,12 @@ class ChatHistoryDatabase:
                 )'''
         )
         # messages are stored as json strings
-        self.cursor.execute(
+        self(
             '''create table if not exists ChatHistory (
                 chat_ID INTEGER PRIMARY KEY,
                 messages TEXT
                 )'''
         )
-        self.connection.commit()
-        self.next_chat_ID = 0
-        self.next_user_ID = 0
 
     def get_next_chat_ID(self):
         self.next_chat_ID += 1
@@ -114,10 +118,9 @@ class ChatHistoryDatabase:
 
     def add_message(self, chat_ID, new_message):
         # get the chat history
-        chat_history = self.cursor.execute(f"select messages from ChatHistory where chat_ID={chat_ID}")\
-                            .fetchone()
+        chat_history = self(f"select messages from ChatHistory where chat_ID={chat_ID}").fetchone()
         if not chat_history:
-            raise ValueError("Chat ID does not exist")
+            raise KeyError("Chat ID does not exist")
         try:
             json_chat_history = json.loads(chat_history[0]) if len(chat_history[0]) > 0 else []
         except json.JSONDecodeError:
@@ -125,45 +128,39 @@ class ChatHistoryDatabase:
             json_chat_history = []
         json_chat_history.append(new_message)
         chat_history_str = json.dumps(json_chat_history)
-        self.cursor.execute(f"UPDATE ChatHistory SET messages='{chat_history_str}' where chat_ID={chat_ID}")
-        self.connection.commit()
+        self(f"UPDATE ChatHistory SET messages='{chat_history_str}' where chat_ID={chat_ID}")
 
     def add_chat(self, user_ID, chat_ID, chat_name=""):
-        self.cursor.execute('''INSERT OR IGNORE INTO UserChats (user_ID, chat_ID, chat_name) 
-                                VALUES (?, ?, ?)''', (user_ID, chat_ID, chat_name))
-        self.cursor.execute(f"INSERT OR IGNORE INTO ChatHistory (chat_ID, messages) VALUES ({chat_ID}, '')")
-        self.connection.commit()
+        self(f"INSERT OR IGNORE INTO UserChats (user_ID, chat_ID, chat_name) VALUES ({user_ID}, {chat_ID}, '{chat_name}')")
+        self(f"INSERT OR IGNORE INTO ChatHistory (chat_ID, messages) VALUES ({chat_ID}, '')")
 
     def get_chat_history(self, chat_ID):
-        return self.cursor.execute(f"select messages from ChatHistory where chat_ID={chat_ID}").fetchone()
+        return self(f"select messages from ChatHistory where chat_ID={chat_ID}").fetchone()
 
     def get_user_chats(self, user_ID):
-        return self.cursor.execute(f"select chat_ID, chat_name from UserChats where user_ID={user_ID}").fetchall()
-    
-    def close(self):
-        self.cursor.close()
-        self.connection.close()
+        return self(f"select chat_ID, chat_name from UserChats where user_ID={user_ID}").fetchall()
 
 # try out database
 if __name__ == "__main__":
-    db = Database("backend/data.db")
+    db = SearchResponseDatabase()
     db.start()
-    db.create_history_table()
+    db.create_table()
     for term, response in [("book flight ticket", "Etihad Airways"), ("find a shopping center", "Dubai Mall")]:
-        db.add_history(term, response)
-    print(1, db("select * from History").fetchall())
+        db.add_search(term, response)
+    print(1, db("select * from Search").fetchall())
     for term, response in [("book flight ticket", ""), ("how to solve numpy error", "stackoverflow website")]:
-        db.add_history(term, response)
-    print(2, db("select * from History").fetchall())
+        db.add_search(term, response)
+    print(2, db("select * from Search").fetchall())
     db.connection.close()
 
-
     db = ChatHistoryDatabase()
+    db.start()
+    db.create_table()
     db.add_chat(1, 1, "test chat")
     for message in example_chat:
         db.add_message(1, message)
     print(json.loads(db.get_chat_history(1)[0]))
-    db.close()
+    db.connection.close()
 
 
     
